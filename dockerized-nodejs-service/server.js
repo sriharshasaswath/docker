8f097cc4d3550fd5cf532c1bfb4cd6d578667fd8
sriharshasaswath <97235759+sriharshasaswath@users.noreply.github.com>
2025-03-24 17:38:58 +0530

    Update server.js

--- dockerized-nodejs-service/server.js
+++ dockerized-nodejs-service/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hellooooooo, world!');
 });
 
 app.get('/secret', basicAuth({
